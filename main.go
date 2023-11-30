0a6904ef54ec942d4a1602d283c94cd565e17cd0
dbugd <0077dk0077@gmail.com>
2023-11-30 15:13:16 +0530

    microseconds

--- main.go
+++ main.go
@@ -12,27 +12,32 @@ import (
 )
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+	// window size = 1 second (1000 milisecond)
+	// events per window = 2
 	limiter1 := slidingwindow.NewLimiter(2, time.Millisecond*1000)
-    fmt.Printf("sliding bucket\n")
+	fmt.Printf("sliding bucket\n")
 	log.Printf("%v\n", limiter1.Allow())
 	log.Printf("%v\n", limiter1.Allow())
 	log.Printf("%v\n", limiter1.Allow())
 	log.Printf("%v\n", limiter1.Allow())
 	log.Printf("%v\n", limiter1.Allow())
 
+	// 2 events per second (frequency)
+	// burst size = 5
 	limiter2 := tokenbucket.NewLimiter(2, 5)
-    fmt.Printf("\ntoken bucket\n")
+	fmt.Printf("\ntoken bucket\n")
 	log.Printf("%v\n", limiter2.Allow())
 	log.Printf("%v\n", limiter2.Allow())
 	log.Printf("%v\n", limiter2.Allow())
 	log.Printf("%v\n", limiter2.Allow())
 	log.Printf("%v\n", limiter2.Allow())
 	log.Printf("%v\n", limiter2.Allow())
-    time.Sleep(time.Second*1)
+	time.Sleep(time.Second * 1)
 	log.Printf("%v\n", limiter2.Allow())
 	log.Printf("%v\n", limiter2.Allow())
 	log.Printf("%v\n", limiter2.Allow())
-    time.Sleep(time.Second*3)
+	time.Sleep(time.Second * 3)
 	log.Printf("%v\n", limiter2.Allow())
 	log.Printf("%v\n", limiter2.Allow())
 	log.Printf("%v\n", limiter2.Allow())
@@ -41,9 +46,11 @@ func main() {
 	log.Printf("%v\n", limiter2.Allow())
 	log.Printf("%v\n", limiter2.Allow())
 
+	// 2 events per second
+	// bucket size = 4
 	wg := sync.WaitGroup{}
 	limiter := leakyBucket.NewLimiter(2, 4)
-    fmt.Printf("\nleaky bucket\n")
+	fmt.Printf("\nleaky bucket\n")
 	for i := 1; i < 10; i++ {
 		t, ok := limiter.Allow()
 		if ok {
